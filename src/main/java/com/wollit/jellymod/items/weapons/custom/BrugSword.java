2af54c5357a500f08f04343b9f5bd50fe476df20
AdamPio <adam.piorkowskiyes@gmail.com>
2023-07-14 01:38:31 +0200

    Brug sword modification

--- src/main/java/com/wollit/jellymod/items/weapons/custom/BrugSword.java
+++ src/main/java/com/wollit/jellymod/items/weapons/custom/BrugSword.java
@@ -4,6 +4,7 @@ import com.wollit.jellymod.items.weapons.AbstractMagicSword;
 import net.minecraft.world.item.Tier;
 
 public class BrugSword extends AbstractMagicSword {
+
     public BrugSword(Tier toolMaterial, int damage, float attackSpeed, Properties properties) {
         super(toolMaterial, damage, attackSpeed, properties);
     }
